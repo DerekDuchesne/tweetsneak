79c2240a4c5ed2aa2d33d768208307d622ff5d07
DerekDuchesne <duchesd1@tcnj.edu>
2015-05-03 02:30:21 -0400

    Added skeleton code for displaying tweets through a search command.

--- tweetsneak_python/main.py
+++ tweetsneak_python/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
+import tweepy
 import sys
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config.from_pyfile("tweetsneak.py")
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -11,10 +12,20 @@ def index():
         return redirect(url_for("search", token=token))
     return render_template("index.html")
 
-@app.route("/search", defaults={"token": ""})
+@app.route("/search/", defaults={"token": ""})
 @app.route("/search/<token>")    
 def search(token):
-    return render_template("search.html", token=token)
+    #TODO: add token to datastore
+    #TODO: get top 10 words
+    
+    auth = tweepy.OAuthHandler(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+    auth.set_access_token(app.config["ACCESS_TOKEN"], app.config["ACCESS_TOKEN_SECRET"])
+    api = tweepy.API(auth)
+    if token == "":
+        tweets = []
+    else:
+        tweets = api.search(q=token)
+    return render_template("search.html", token = token, tweets = tweets)
     
 @app.errorhandler(404)
 def notfound(e):
