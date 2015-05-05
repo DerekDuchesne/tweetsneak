0709b8c458b30935f21d05c09c7ed20563b4b917
DerekDuchesne <duchesd1@tcnj.edu>
2015-05-05 01:16:07 -0400

    Added functionality to count top 10 words and paginate through tweets. Updated UI and switched Twitter APIs.

--- tweetsneak_python/main.py
+++ tweetsneak_python/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, url_for, request, redirect
-import tweepy
+from collections import Counter
+from twitter import *
+import json
+import math
 import sys
 
 app = Flask(__name__)
@@ -12,20 +15,35 @@ def index():
         return redirect(url_for("search", token=token))
     return render_template("index.html")
 
-@app.route("/search/", defaults={"token": ""})
-@app.route("/search/<token>")    
+@app.route("/search/", defaults={"token": ""}, methods = ["GET", "POST"])
+@app.route("/search/<token>", methods = ["GET", "POST"])    
 def search(token):
+    if request.method == "POST":
+        token = request.form["token"]
+        return redirect(url_for("search", token=token))
+        
     #TODO: add token to datastore
-    #TODO: get top 10 words
+    BEARER_TOKEN = oauth2_dance(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
+    t = Twitter(auth=OAuth2(bearer_token = BEARER_TOKEN))
     
-    auth = tweepy.OAuthHandler(app.config["CONSUMER_KEY"], app.config["CONSUMER_SECRET"])
-    auth.set_access_token(app.config["ACCESS_TOKEN"], app.config["ACCESS_TOKEN_SECRET"])
-    api = tweepy.API(auth)
     if token == "":
         tweets = []
     else:
-        tweets = api.search(q=token)
-    return render_template("search.html", token = token, tweets = tweets)
+        tweets = t.search.tweets(q=token, count=app.config["MAX_TWEETS"])["statuses"]
+    
+    transtab = dict((ord(char), None) for char in u"-=+|!@#$%^&*()`~[]{};:'\",<.>\\/?")
+    word_list = Counter()
+    
+    for tweet in tweets:
+        for word in tweet["text"].translate(transtab).split():
+            word_list[word.lower()] += 1
+            
+    most_common = enumerate(word_list.most_common(10))
+    num_pages = int(math.ceil(float(len(tweets)) / app.config["RPP"]))
+    if num_pages == 0:
+        num_pages = 1
+            
+    return render_template("search.html", token = token, tweets = map(json.dumps, tweets), most_common = most_common, rpp = app.config["RPP"], num_pages = num_pages)
     
 @app.errorhandler(404)
 def notfound(e):
